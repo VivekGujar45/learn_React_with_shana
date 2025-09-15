4e64fadb61a1afe8fd80040520c2df51a7d07929
VivekGujar45 <gujarvivek32@gmail.com>
2025-09-15 17:02:20 +0530

    werv

--- 01basicreactproj/src/index.js
+++ 01basicreactproj/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> //renders the app which is a function
   </React.StrictMode>
 );
 

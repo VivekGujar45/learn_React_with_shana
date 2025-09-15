39fd8126b2714d27b94e4649a470c580db769b5b
VivekGujar45 <gujarvivek32@gmail.com>
2025-09-15 17:30:29 +0530

    asdfg

--- 01basicreactproj/src/App.js
+++ 01basicreactproj/src/App.js
@@ -1,8 +1,12 @@
+import Shana from "./shana"
 function App() {
   return (
+    <> 
+    <Shana /> 
     <h1>
-      Code with Shana | React Tutorial
+      {/* Code with Shana | React Tutorial */}
     </h1>
+    </>
   );
 }
 

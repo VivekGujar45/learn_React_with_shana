421969c399ca60aada0d07dbcc4d6e7e259abdd3
VivekGujar45 <gujarvivek32@gmail.com>
2025-09-16 16:06:22 +0530

    qwerfv

--- bgChangerProj/src/App.jsx
+++ bgChangerProj/src/App.jsx
@@ -11,7 +11,24 @@ function App() {
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">test
         <div className="flex flex-wrap justify-center gap-4 bg-white/30 shadow-x px-3 py-2 rounded-3xl"> test
           <button
-          className="outline-none px-4 "> red</button>
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 roundded-full text-white shadow-lg"
+          style = {{backgroundColor: "red"}}> red</button>
+
+          <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 roundded-full text-white shadow-lg"
+          style = {{backgroundColor: "green"}}> green</button>
+
+          <button
+          onClick={() => setColor("blue")}
+          className="outline-none px-4 py-1 roundded-full text-white shadow-lg"
+          style = {{backgroundColor: "blue"}}> blue</button>
+
+          <button
+          onClick={() => setColor("orange")}
+          className="outline-none px-4 py-1 roundded-full text-white shadow-lg"
+          style = {{backgroundColor: "orange"}}> orange</button>
       </div>
 
     </div>

2e0a7dbb8db6bff5aedfcb8c176ce95986d32eee
VivekGujar45 <gujarvivek32@gmail.com>
2025-09-16 15:39:23 +0530

    trfds

--- bgChangerProj/src/App.jsx
+++ bgChangerProj/src/App.jsx
@@ -1,12 +1,18 @@
+import { use } from "react"
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color,setColor] = useState(olive)
 
   return (
-    <>
-      
-    </>
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor:color}}> 
+
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">test
+        
+      </div>
+
+    </div>
   )
 }
 
